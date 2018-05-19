8c801e74b6108a68c18f2f4a294edabecc9131a3
Aaron Tang <aaronwt@berkeley.edu>
2018-05-19 16:13:29 -0700

    added the sprites as characters, have temporary 'finish'

--- BandOfShapesRE/Assets/Scripts/MoveToPoint.cs
+++ BandOfShapesRE/Assets/Scripts/MoveToPoint.cs
@@ -6,6 +6,7 @@ using UnityEngine.AI;
 public class MoveToPoint : MonoBehaviour
 {
     NavMeshAgent agent;
+	public LineRenderer lineRenderer;
 
     [HideInInspector]
     public bool selected = false;
@@ -15,6 +16,9 @@ public class MoveToPoint : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         rend = GetComponent<Renderer>();
+		CreateCircleAroundPoint ();
+		lineRenderer.widthMultiplier = 0.25f;
+		lineRenderer.enabled = false;
     }
 
     void Update()
@@ -25,8 +29,10 @@ public class MoveToPoint : MonoBehaviour
 
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
-                if (hit.collider.gameObject.tag == "Terrain")
-                    selected = false;
+				if (hit.collider.gameObject.tag == "Terrain")
+				{
+					selected = false;
+				}
             }
         }
         if (Input.GetMouseButtonDown(1) && selected)
@@ -36,6 +42,7 @@ public class MoveToPoint : MonoBehaviour
             if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
             {
                 agent.destination = hit.point;
+				agent.updateRotation = false;
             }
         }
 
@@ -55,11 +62,40 @@ public class MoveToPoint : MonoBehaviour
         if (!selected)
         {
             selected = true;
+			lineRenderer.enabled = true;
         }
         else
         {
             selected = false;
+			lineRenderer.enabled = false;
         }
     }
+
+	void CreateCircleAroundPoint ()
+	{
+		float x;
+		float y = 0.0f;
+		float z;
+
+		float xradius = 1.0f;
+		float zradius = 1.0f;
+
+		float angle = 0.0f;
+
+		int segments = 36;
+
+		lineRenderer.SetVertexCount (segments + 1);
+		lineRenderer.useWorldSpace = false;
+
+		for (int i = 0; i < segments + 1; i++)
+		{
+			x = Mathf.Sin (Mathf.Deg2Rad * angle) * xradius;
+			z = Mathf.Cos (Mathf.Deg2Rad * angle) * zradius;
+
+			lineRenderer.SetPosition (i, new Vector3(x,y,z) );
+
+			angle += (360f / segments);
+		}
+	}
 
 }
