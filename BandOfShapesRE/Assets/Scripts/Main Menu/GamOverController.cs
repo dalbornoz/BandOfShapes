a25ec47ba092e1ebfb96e97955cd34c26a1b668e
Unknown <btatnguyen@gmail.com>
2018-05-20 16:04:33 -0700

    esfwaf
    fafdwad

--- BandOfShapesRE/Assets/Scripts/Main Menu/GamOverController.cs
+++ BandOfShapesRE/Assets/Scripts/Main Menu/GamOverController.cs
@@ -4,16 +4,17 @@ using UnityEngine;
 
 public class GamOverController : MonoBehaviour {
 
-    public Transform gameOverCanvas;
+    public Transform canvas;
     public GameObject triangle;
     public GameObject square;
     public GameObject circle;
 	
 	// Update is called once per frame
 	void Update () {
-		if (!triangle.activeSelf || !square.activeSelf || !circle.activeSelf)
+		if (triangle.activeSelf == false || square.activeSelf == false || circle.activeSelf == false)
         {
-            gameOverCanvas.gameObject.SetActive(true);
+            Debug.Log("Game Over");
+            canvas.gameObject.SetActive(true);
             Time.timeScale = 0;
         }
 	}
