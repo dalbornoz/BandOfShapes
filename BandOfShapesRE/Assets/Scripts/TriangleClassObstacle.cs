d39bac52fcbb5e5aa35272f5699324a058b5a991
Unknown <dalborno@uci.edu>
2018-05-19 18:57:57 -0700

    lol

---
+++ BandOfShapesRE/Assets/Scripts/TriangleClassObstacle.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TriangleClassObstacle : MonoBehaviour {
+    public Transform exit;
+    // Use this for initialization
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Triangle"))
+        {
+            other.gameObject.transform.position = exit.position + new Vector3(1.0f, 0, 1.0f);
+        }
+    }
+}
---
+++ BandOfShapesRE/Assets/Scripts/TriangleEntrance.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class TriangleEntrance : MonoBehaviour {
+    public Transform exit;
+    // Use this for initialization
+    //
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Triangle"))
+        {
+            other.gameObject.transform.position = exit.position;
+            other.gameObject.GetComponent<NavMeshAgent>().destination = other.gameObject.transform.position;
+        }
+    }
+}
