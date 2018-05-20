83bc30247cc6d9b2e44eb5f05b897cead479222c
Unknown <zacloutier@gmail.com>
2018-05-19 18:47:00 -0700

    Auto stash before merge of "master" and "origin/master"

--- BandOfShapesRE/Assets/Scripts/Interactables/PressureButton.cs
+++ BandOfShapesRE/Assets/Scripts/Interactables/PressureButton.cs
@@ -1,15 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class PressureButton : MonoBehaviour {
 
     public bool isTriggered;
+    private OffMeshLink oof;
 
 	// Use this for initialization
 	void Start () {
+        oof = GetComponent<OffMeshLink>();
 		isTriggered = false;
 	}
+
+    private void Update()
+    {
+        if (isTriggered)
+            oof.activated = true;
+        else
+            oof.activated = false;
+    }
 
     //Triggers while any player object is on the trigger collider
     private void OnTriggerStay(Collider other)
