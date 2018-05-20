97f4971fbe9f2802a80be72f6a802013497d5f9d
Unknown <btatnguyen@gmail.com>
2018-05-20 15:45:01 -0700

    spikes for real this time

---
+++ BandOfShapesRE/Assets/Scripts/Main Menu/GamOverController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamOverController : MonoBehaviour {
+
+    public Transform gameOverCanvas;
+    public GameObject triangle;
+    public GameObject square;
+    public GameObject circle;
+	
+	// Update is called once per frame
+	void Update () {
+		if (!triangle.activeSelf || !square.activeSelf || !circle.activeSelf)
+        {
+            gameOverCanvas.gameObject.SetActive(true);
+            Time.timeScale = 0;
+        }
+	}
+}
