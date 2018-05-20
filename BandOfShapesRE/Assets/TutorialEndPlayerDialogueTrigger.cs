f7f880ed73e8fb6a18db8b7f221dd7574dbf16bd
Aaron Tang <aaronwt@berkeley.edu>
2018-05-20 13:37:55 -0700

    Added all dialogue triggers to Tutorial Level (on my own tutorial scene)

---
+++ BandOfShapesRE/Assets/TutorialEndPlayerDialogueTrigger.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TutorialEndPlayerDialogueTrigger : PlayerDialogueTrigger {
+
+	public List<GameObject> playersInFinish;
+
+	public override void OnTriggerEnter (Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Player"))
+		{
+			playersInFinish.Add (other.gameObject);
+		}
+	}
+
+	void OnTriggerExit (Collider other)
+	{
+		if (other.gameObject.CompareTag ("Player"))
+		{
+			if (playersInFinish.Contains (other.gameObject))
+			{
+				playersInFinish.Remove (other.gameObject);
+			}
+		}
+	}
+
+	void Update ()
+	{
+		if (playersInFinish.Count == 3)
+		{
+			if (!dialogueTriggered)
+			{
+				StartCoroutine(StartDialogue ());
+			}
+		}
+	}
+
+
+	public override IEnumerator StartDialogue() 
+	{
+		dialogueTriggered = true;
+
+		for (int i = 0; i < anims.Length; i++)
+		{
+			if (i != 0) // Only wait after the first dialogue line
+			{
+				yield return new WaitForSeconds (2.0f);
+			}
+
+			CharacterDialogueManager characterDialogueManager = characterDialogueManagers [i];
+			Animator anim = anims [i];
+			string dialogueLine = dialogueLines [i];
+			characterDialogueManager.SetDialogue (dialogueLine);
+			anim.SetTrigger ("playDialogue");
+		}
+		yield return new WaitForSeconds (4.0f);
+		SceneManager.LoadScene ("AaronScene"); // Change the next scene to be the next level
+	}
+}
