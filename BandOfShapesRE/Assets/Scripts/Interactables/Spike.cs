3dc845fa35a9fd474154242d33144a3b2a943a2a
Unknown <btatnguyen@gmail.com>
2018-05-20 13:16:51 -0700

    plz plz plz nothing break

---
+++ BandOfShapesRE/Assets/Scripts/Interactables/Spike.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spike : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" || other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
